23dcf2b644e55ff9322f5a172a3eb4f1453d8e43
DistMinds <129870151+DistMinds@users.noreply.github.com>
2024-09-12 21:35:27 +0800

    Update sort.py

--- libraries_np/np_sort/sort.py
+++ libraries_np/np_sort/sort.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 
 def handler(event, context=None):
-    start = time()
+    start =  GetTime()
     size = 1000
     a= np.array([19, 12,  8, 15, 13, 19, 14, 19, 15, 15, 12,  6, 15, 12,  4,  8])
     result = np.sort(a)
-    latency = (time()-start)*1000
+    latency = ( GetTime()-start)*1000
     return latency
 
 @app.route('/event-invoke', methods = ['POST'])
